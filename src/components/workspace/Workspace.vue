b557ab97db802094f16a76a3c4ebb428faac27fa
Caio Queiroz <caiogqueiroz@gmail.com>
2019-02-26 11:44:23 -0300

    running task ui

--- src/components/workspace/Workspace.vue
+++ src/components/workspace/Workspace.vue
@@ -1,8 +1,27 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <v-layout row>
+    <!-- Running Task -->
+    <v-layout row v-if="taskRunning">
       <v-flex xs12>
-        <v-card white>Iniciar Tarefa / Mostrar tarefa em execucao</v-card>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-avatar>
+              <v-icon :color="runningTask.project.color">folder</v-icon>
+            </v-list-tile-avatar>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ runningTask.description }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ runningTask.project.name }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-list-tile-action-text>{{ runningTimer | secondsToTime }}</v-list-tile-action-text>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">pause</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
@@ -14,7 +33,6 @@
       <v-flex lg5 md6 xs12>
         <v-card white>Trabalhando nisso?</v-card>
       </v-flex>
-      
     </v-layout>
     <app-new-task></app-new-task>
   </v-container>
@@ -23,12 +41,39 @@
 <script>
 import Task from "./Task.vue";
 import NewTask from "./NewTask.vue";
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      taskRunning: null,
+      timer: null,
+      isRunning: false
+    };
+  },
   computed: {
     tasks() {
       return this.$store.getters.tasksDone;
+    },
+    runningTask() {
+      return this.taskRunning;
+    },
+    runningTimer() {
+      return this.timer;
+    }
+  },
+  filters: {
+    secondsToTime: function(value) {
+      let hours = parseInt(Math.floor(value / 360));
+      let minutes = parseInt(Math.floor((value - hours * 360) / 60));
+      let seconds = parseInt((value - (hours * 360 + minutes * 60)) % 60);
+
+      let dHours = hours > 9 ? hours : "0" + hours;
+      let dMins = minutes > 9 ? minutes : "0" + minutes;
+      let dSecs = seconds > 9 ? seconds : "0" + seconds;
+
+      return dHours + ":" + dMins + ":" + dSecs;
     }
   },
   components: {
@@ -36,10 +81,33 @@ export default {
     appNewTask: NewTask
   },
   methods: {
-    ...mapActions(["loadTasks"])
+    ...mapActions(["loadTasks"]),
+    runTask() {
+      setInterval( () => {
+        //console.log(this.isRunning);
+        if (this.isRunning) {
+          this.timer++;
+        }
+      }, 1000);
+    }
   },
-  mounted () {
-    //Load Tasks of User
+  mounted() {
+    // Load running task
+    axios
+      .get("/workspace/5c58e119d5e2b359800929a1/running-task")
+      .then(task => {
+        if (task.status !== 200) {
+          throw new Error("Falha ao obter tarefa em execução.");
+        }
+        this.taskRunning = task.data;
+        this.timer = task.data.timer;
+        this.isRunning = true;
+        this.runTask();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // Load Tasks of User
     this.loadTasks();
   }
 };
